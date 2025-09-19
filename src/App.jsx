0a3e91b3335a31164cd4154e955b13fb39abf554
felipe.chagas <felipe.chagas@sga.pucminas.br>
2025-09-19 13:53:59 +0000

    Changes before Firebase Studio auto-run

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,61 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [nome, setNome] = useState('Felipe')
+  let clique = 1
+
+  const handleNomeClick = () => {
+    // Verifica o valor ATUAL do nome e define o NOVO valor
+    if (clique % 2 === 0) {
+      clique++;
+      setNome('Funcionou :D');
+    } else {
+      setNome('Felipe');
+    }
+  }
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="c_titulo">Aula de Reactinho</h1>
+      <a href="https://www.google.com.br">SITE DO GOOGLE :)</a>
+      <div className="c_corbranca">
+        <ol>
+          <li>DOMINGO</li>
+          <li>SEGUNDA</li>
+        </ol>
+
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>NOME</th>
+              <th>IDADE</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>{nome}</td>
+              <td>25</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div>
+        <button type="button" onClick={handleNomeClick}>
+          JS
+        </button>
+      </div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          apertou {count} vezes
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
